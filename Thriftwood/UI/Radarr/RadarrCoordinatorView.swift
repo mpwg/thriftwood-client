93ca969b1f79ca09f395faf2dbf4a446d6b3275e
Matthias Wallner-Géhri <68284225+mpwg@users.noreply.github.com>
2025-10-06 19:19:30 +0200

    todos

--- Thriftwood/UI/Radarr/RadarrCoordinatorView.swift
+++ Thriftwood/UI/Radarr/RadarrCoordinatorView.swift
@@ -63,7 +63,7 @@ struct RadarrCoordinatorView: View {
             let viewModel = coordinator.getMovieDetailViewModel(movieId: movieId)
             MovieDetailView(
                 onEdit: { movieId in
-                    coordinator.showMovieDetail(movieId: movieId) // TODO [M2-Future]: Navigate to edit when implemented
+                    coordinator.showMovieDetail(movieId: movieId) // TODO [#204]: Navigate to edit when implemented
                 },
                 onDeleted: {
                     coordinator.pop()
@@ -114,15 +114,15 @@ struct RadarrCoordinatorView: View {
             }
             
         case .systemStatus:
-            // TODO [M2-Future]: Implement system status view
+            // TODO [#205]: Implement system status view
             placeholderView(title: "System Status", icon: "info.circle")
             
         case .queue:
-            // TODO [M2-Future]: Implement queue view
+            // TODO [#205]: Implement queue view
             placeholderView(title: "Queue", icon: "arrow.down.circle")
             
         case .history:
-            // TODO [M2-Future]: Implement history view
+            // TODO [#205]: Implement history view
             placeholderView(title: "History", icon: "clock")
         }
     }
